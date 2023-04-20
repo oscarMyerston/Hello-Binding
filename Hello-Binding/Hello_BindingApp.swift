5fa45f059da1b40df3df704b425d27c5394dc37c
Oscar David Myerston Vega <oscar.myerston@wiedii.co>
2023-04-20 18:31:15 -0500

    Initial Commit

---
+++ Hello-Binding/Hello_BindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_BindingApp.swift
+//  Hello-Binding
+//
+//  Created by Oscar David Myerston Vega on 20/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Hello_BindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
