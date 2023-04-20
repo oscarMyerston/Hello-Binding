e25f8aec1a757aeb8512a0191ccf0c4e72575d69
Oscar David Myerston Vega <oscar.myerston@wiedii.co>
2023-04-20 18:58:39 -0500

    first commit

--- Hello-Binding/ContentView.swift
+++ Hello-Binding/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    var model = Dish.all()
+    @State private var isSpicy: Bool = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List {
+            Toggle(isOn: $isSpicy) {
+                Text("Spicy")
+                    .font(.title)
+            }
+
+            ForEach(model.filter { $0.isSpicy == self.isSpicy }) { dish in
+                HStack {
+                    Image(dish.imageURL)
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                    Text(dish.name)
+                        .font(.title)
+                        .lineLimit(nil)
+                    Spacer()
+                    if (dish.isSpicy) {
+                        Image("spicy-icon")
+                            .resizable()
+                            .frame(width: 35, height: 35)
+                    }
+                }
+
+            }
         }
-        .padding()
     }
 }
 
---
+++ Hello-Binding/Models/Dish.swift
@@ -0,0 +1,30 @@
+//
+//  Dish.swift
+//  Hello-Binding
+//
+//  Created by Oscar David Myerston Vega on 20/04/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct Dish: Identifiable {
+
+    let id = UUID()
+    let name: String
+    let imageURL: String
+    let isSpicy: Bool
+
+}
+
+extension Dish {
+
+    static func all() -> [Dish] {
+
+        return [
+            Dish(name: "Kung Pow Chicken", imageURL: "kungpow", isSpicy: true),
+            Dish(name: "Sweet and Sour Chicken", imageURL: "sweet", isSpicy: false),
+            Dish(name: "Spicy Red Chicken", imageURL: "spicy", isSpicy: true)
+        ]
+    }
+}
